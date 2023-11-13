feac966ab447867495a7c3c5e1607f826bf9895a
Ubuntu <ubuntu@ip-172-31-17-38.eu-north-1.compute.internal>
2023-11-13 14:35:30 +0000

    change phone filter

--- src/bot/filters/phone.py
+++ src/bot/filters/phone.py
@@ -5,11 +5,13 @@ from aiogram.types import Message
 class PhoneFilter(BaseFilter):
     async def __call__(self, message: Message):
         phone = message.text
-        if phone.isdigit() and len(phone) == 12:
+        if phone[0]=='+' and phone[1:].isdigit() and len(phone) == 12:
+            return True
+        elif phone.isdigit() and len(phone) == 12:
             return True
         elif phone.isdigit() and len(phone) == 10:
             return True
         else:
-            text = "Введіть телефон в одному з заданих форматів:\n`380686667788`\nабо `0686667788`"
+            text = "Введіть телефон в одному з заданих форматів:\n`+380686667788`\nабо `380686667788`\nабо `0686667788`"
             await message.answer(text, parse_mode="Markdown")
             return False
